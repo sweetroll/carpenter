102119ecd40765f412494457207f7964f45a8919
Matt Cordell <matt@evolution7.com.au>
2015-12-09 13:25:12 +0100

    Basic folder structure and composer dependencies

---
+++ config/application.php
@@ -0,0 +1,19 @@
+<?php
+$root_dir = dirname(__DIR__);
+$webroot_dir = $root_dir . '/public';
+
+/**
+ * Use Dotenv to set required environment variables and load .env file in root
+ */
+$dotenv = new Dotenv\Dotenv($root_dir);
+if (file_exists($root_dir . '/.env')) {
+  $dotenv->load();
+  $dotenv->required([
+      'DB_NAME', 'DB_USER', 'DB_PASSWORD',
+      'CRAFT_USERNAME', 'CRAFT_EMAIL', 'CRAFT_PASSWORD',
+      'CRAFT_SITENAME', 'CRAFT_SITEURL', 'CRAFT_LOCALE'
+  ]);
+}
+
+// TODO
+// - Write DB credentials to db.php
