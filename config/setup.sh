732a40c70142725631aacdf8888962a24f5396d0
Matt Cordell <matt@evolution7.com.au>
2016-02-05 15:13:35 +1100

    removed force on import

--- config/setup.sh
+++ config/setup.sh
@@ -2,4 +2,4 @@
 composer install
 vendor/bin/craft install # (agree to license)
 cp config/db.php craft/config/db.php
-(eval `cat .env`; export DB_HOST; export DB_NAME; export DB_USER; export DB_PASSWORD; export CRAFT_USERNAME; export CRAFT_EMAIL; export CRAFT_PASSWORD; export CRAFT_SITENAME; export CRAFT_SITEURL; export CRAFT_LOCALE; php -d variables_order=EGPCS ./vendor/bin/schematic import index --file=config/schema.yml --force)
+(eval `cat .env`; export DB_HOST; export DB_NAME; export DB_USER; export DB_PASSWORD; export CRAFT_USERNAME; export CRAFT_EMAIL; export CRAFT_PASSWORD; export CRAFT_SITENAME; export CRAFT_SITEURL; export CRAFT_LOCALE; php -d variables_order=EGPCS ./vendor/bin/schematic import index --file=config/schema.yml)
